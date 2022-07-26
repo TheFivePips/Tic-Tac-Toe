7ed3aa6e66df2d3d8aa66de4c2d90cc0d5311f4c
sam gardner <samgardner20@gmail.com>
2022-07-26 09:52:51 -0400

    a functioning game of tic tac toe with two live players clicking back and forth. still no ability to check for a win

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
+// any logic related to players
+// use this to add an extra player later on
+const Player = (name) => {
+
+    let score = 0 
+    let isPlayerTurn = false
+
+    // const swapTurn = function(isPlayerTurn){
+    //     return !isPlayerTurn
+    // }
+
+    return { name, score, isPlayerTurn } 
+
+
+}
+
+const player1 = Player("player1")
+player1.mark = "X"
+player1.isPlayerTurn = true
+
+const player2 = Player("player2")
+player2.mark = "O"
+
+
+
 // any logic related to the board or position of selections
 const gameboard = (() =>{
     const board = document.querySelector('.board')
     const selection = ["","","","","","","","","",]
-
+    
     // loop through the game selection array and for each item, create a div with an inner html correpsonding to the value. give each item a class of selection append that div to the board
     const displayBoard = function() {
        
@@ -11,8 +36,17 @@ const gameboard = (() =>{
             item.setAttribute('class', 'selection')
             item.setAttribute('id', i)
             item.addEventListener('click', function() {
-                console.log("clicked")
-                item.innerHTML = item.id
+                if(player1.isPlayerTurn) {
+                    item.innerHTML = player1.mark
+                    player1.isPlayerTurn = false
+                    player2.isPlayerTurn = true
+                }
+                else if(player2.isPlayerTurn) {
+                    item.innerHTML = player2.mark
+                    player2.isPlayerTurn = false
+                    player1.isPlayerTurn = true
+                }
+
             })
              board.append(item)
             }
@@ -31,24 +65,7 @@ gameboard.displayBoard()
 
 
 // any logic related to the flow of the game
-const game = (()=>{
+// const game = (()=>{
 
-})()
+// })()
 
-
-// any logic related to players
-// use this to add an extra player later on
-const Player = (name) => {
-
-    let score = 0 
-    let isPlayerTurn = false
-
-    return { name, score, isPlayerTurn } 
-
-
-}
-
-
-
-const player1 = Player(player1)
-player1.mark = "X"
